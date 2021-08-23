3f8bcc125c1b4555e76d925e90483b8a705f5574
Gleb <glebko123@gmail.com>
2021-08-23 20:49:51 +0300

    struct within struct

--- structs/main.go
+++ structs/main.go
@@ -2,13 +2,25 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 type person struct {
 	firstName string
-	lastName string
+	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	alex := person{firstName: "Alex",lastName: "Anderson"}
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contact: contactInfo{
+			email:   "alex@example.com",
+			zipCode: 22230,
+		},
+	}
 	fmt.Println(alex)
 	var alex2 person
 	alex2.firstName = "Alexander"
